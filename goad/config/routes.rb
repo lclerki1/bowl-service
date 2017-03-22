fed4e57fafb05fe2be88c240185e284b7870a597
lclerki1 <lauren.m.copeland@gmail.com>
2017-03-22 16:27:16 -0400

    added player table and a few seeded players

--- goad/config/routes.rb
+++ goad/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :players
   get 'goad/welcome'
 
   root 'goad#welcome'
--- goad/db/seeds.rb
+++ goad/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.distroy_all
+
+Player.create! [
+  { first_name: "Lauren", last_name: "Clerkin", gender: "female" },
+  { first_name: "Erin", last_name: "Hitt", gender: "female" },
+  { first_name: "Susan", last_name: "Hoffmaster", gender: "female" },
+  { first_name: "Shawn", last_name: "Polk", gender: "male" },
+  { first_name: "Chuck", last_name: "Davis", gender: "male" }
+]
