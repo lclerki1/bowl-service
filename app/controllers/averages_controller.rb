af5e98a4f9becf96369e9bde12bbd6faa64d4539
lclerki1 <lauren.m.copeland@gmail.com>
2017-04-24 21:55:46 -0400

    averages can be filtered by months and year selection

--- app/controllers/averages_controller.rb
+++ app/controllers/averages_controller.rb
@@ -2,30 +2,56 @@ class AveragesController < ApplicationController
   
   def compare
     @people = Person.all
-    if params[:date]
-      @date = Date.parse(params[:date])
+    
+    if params[:week]
+      @date = Date.parse(params[:week])
+    elsif params[:date]
+      @date = params[:date].values
+      @year = @date[0].to_i
+      @sMonth = @date[1].to_i
+      @eMonth = @date[2].to_i
     else
       @date = Date.today
+      @year = Date.today.year
+      @sMonth = Date.today.month
+      @eMonth = Date.today.month
     end
     
-    @time = [@date.beginning_of_week(:sunday), @date.end_of_week(:saturday)]
     @bowlers = []
+    
     @people.each do |person|
+      @bowler = {}
+      @avgs = []
+      @dates = [] 
+      if @date == Date.today
+        @time = [@date.beginning_of_week(:sunday), @date.end_of_week(:sunday)]
+        @time_games = person.game_sets.where(date: (@time[0]..@time[1]))
+      elsif params[:week]
+        @time = [@date.beginning_of_week(:sunday), @date.end_of_week(:sunday)]
+        @time_games = person.game_sets.where(date: (@time[0]..@time[1]))
+      else
+        @time_games = person.game_sets.where(date: (Date.new(@year, @sMonth, 1)..Date.new(@year, @eMonth, -1)))
+      end
       
-      @time_games = person.game_sets.where(date: (@time[0]..@time[1]))
-      @time_games.each do |g|
-        @bowler = {}
+      @time_games.each do |set|
         @bowler["id"] = person.id
         @bowler["first_name"] = person.first_name
         @bowler["last_name"] = person.last_name
-        @bowler["avg"] = g.average
-        @bowler["date"] = g.date
-        @bowler["day"] = Date::DAYNAMES[g.date.wday]
+        @avgs << set.average
+        @dates << set.date          
+      end
+      
+      if @avgs.length >= 1
+        @max_avg_index = @avgs.each_with_index.max[1]
+        @bowler["avg"] = @avgs[@max_avg_index]
+        @bowler["date"] = @dates[@max_avg_index]
+        @bowler["day"] = Date::DAYNAMES[@dates[@max_avg_index].wday]
         @bowlers << @bowler
       end
     end
-    
-    @sorted = @bowlers.sort_by { |k| k["avg"] }.reverse    
+    if @bowlers.length >= 1
+      @sorted = @bowlers.sort_by { |k| k["avg"] }.reverse   
+    end 
   end
   
 end
